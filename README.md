1d3e6ea59b25dd33bdb19ba36b89a2abc642c6fd
Max Rosorani <m.rosorani@gmail.com>
2023-07-29 14:44:13 +0200

    Add scripts and readme changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# target-alert-price
+# Target price alert
+
+### Google Sheet configuration
+![](google_sheets_image.png)
+
+Blue fields needs to be filled by the user, white ones are manager automatically.
+Checkbox can be disabled/enabled also manually
+
+### Enable trigger 
+
+Enable trigger on "checkPrices" function 
---
+++ apps_script.js
@@ -0,0 +1,87 @@
+const columnName                  = 0;
+const columnTicker                = 1;
+const columnTargetPrice           = 2;
+const columnTargetPercentage      = 3;
+const columnStrategy              = 4;
+const columnCurrentPrice          = 5;
+const columnCurrentDiffPercentage = 6;
+const columnEmailSent             = 7;
+
+let bodyMessage = '';
+let sendMessage = false;
+let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Prices");
+
+function AddStrategyRowToEmail(row, i) {
+  sendMessage = true;
+  bodyMessage += `<strong>${row[columnName]}</strong>: current price is €${row[columnCurrentPrice].toFixed(2)} 
+    and your strategy is ${row[columnStrategy].toUpperCase()}<br>`;
+  i++;
+  // stop sending message after the first one
+  sheet.getRange(`H${i}`).setValue(true);
+}
+
+function AddPercentageRowToEmail(row, i) {
+  sendMessage = true;
+  bodyMessage += `<strong>${row[columnName]}</strong>: current price is €${row[columnCurrentPrice].toFixed(2)} 
+    and is ${row[columnTargetPercentage]*100}% around your target price<br>`;
+  i++;
+  // stop sending message after the first one
+  sheet.getRange(`H${i}`).setValue(true);
+}
+
+function checkPrices() {
+  // Pulls data from the spreadsheet
+  let source = sheet.getRange("A:H");
+  let data = source.getValues();
+
+  //Loops through the cells in the spreadsheet to find cells where the stock fell below purchase price
+  let n = 0;
+  for (let i in data) {
+    //Skips the first row
+    if (n++ == 0) continue;
+
+    //Loads the current row
+    let row = data[i];
+
+    //Once at the end of the list, exits the loop
+    if (row[columnTicker] == "") break;
+
+    // if mail has been sent, go to next line
+    if (row[columnEmailSent]) continue;
+
+    // DEPENDING ON THE STRATEGY
+    if (row[columnStrategy] == 'Lower than') {
+      // if price is lower than target price
+      if (row[columnCurrentPrice] <= row[columnTargetPrice]) {
+        AddStrategyRowToEmail(row, i);
+        continue;
+      }   
+    } else if (row[columnStrategy] == 'Upper than') {
+      // if price is upper than target price
+      if (row[columnCurrentPrice] >= row[columnTargetPrice]) {
+        AddStrategyRowToEmail(row, i);
+        continue;
+      }
+    }
+
+    // if currente percentage diff is around target percentage
+    if (Math.abs(row[columnCurrentDiffPercentage]) < row[columnTargetPercentage]) {
+        AddPercentageRowToEmail(row, i);
+        continue;
+    }
+  }
+
+  if (!sendMessage) return;
+
+  sendEmail(bodyMessage);
+}
+
+function sendEmail(bodyMessage) {
+  const intro = "<h3>Price alert</h3>";
+  // console.log(intro + bodyMessage)
+  MailApp.sendEmail({
+    to: SpreadsheetApp.getActiveSpreadsheet().getOwner().getEmail(),
+    subject: "Target price alert",
+    htmlBody: intro + bodyMessage,
+  });
+}
