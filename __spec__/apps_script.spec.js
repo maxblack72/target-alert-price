cd183476cf9d930b016f809acdb9f82d49940ad5
Max Rosorani <m.rosorani@gmail.com>
2023-08-03 22:58:40 +0200

    Trying to add second test

--- __spec__/apps_script.spec.js
+++ __spec__/apps_script.spec.js
@@ -8,14 +8,16 @@ global.SpreadsheetApp = {
       getRange: (range) => ({
         getValues: jest.fn((name) => [
           [],
-          ['Etherium', 'ETH-EUR',  999, 5.00, 'Buy', 1000, 1.3, false],
-          // ['Palantir', 'NYSE:PLTR',	14.30,  3.00,	'Buy',	19.98,	39.72,	false]
+          ['Etherium', 'ETH-EUR',  800, 4.00, 'Buy', 1000, 1.8, false],
+          // ['Etherium', 'ETH-EUR',  800, 5.00, 'Sell', 1000, 10, false],
         ]),
         setValue: jest.fn((boolValue) => true)
       })
     }),
   }),
 };
+
+
 global.columnName                  = 0;
 global.columnTicker                = 1;
 global.columnTargetPrice           = 2;
@@ -33,6 +35,7 @@ let {
 
 describe('checkPrices', () => {
   afterEach(() => {
+    jest.restoreAllMocks();
   });
   beforeEach(() => {
     global.MailApp = {
@@ -50,5 +53,18 @@ describe('checkPrices', () => {
     checkPrices(); // non esegue la moccata ma l'originale
     expect(mockAddPercentageRowToEmail).toHaveBeenCalled();
   });
+
+
+  // test('should pull data from the "Prices" sheet1', () => {
+  //   // Call the checkPrices to be tested
+  //   // console.log(addPercentageRowToEmail.toString());
+  //   const mockAddStrategyRowToEmail = jest.fn((row, i) => null);
+  //   addStrategyRowToEmail = mockAddStrategyRowToEmail;
+
+  //   // addPercentageRowToEmail(); //esegue la moccata ( il test diventa ok)
+  //   checkPrices(); // non esegue la moccata ma l'originale
+  //   expect(mockAddStrategyRowToEmail).toHaveBeenCalled();
+  // });
+
 });
 
